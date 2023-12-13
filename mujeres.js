f0e00a41cbc88113c75dedb4797905b2f86f9d14
pancho040 <131422573+pancho040@users.noreply.github.com>
2023-12-13 04:52:16 -0400

    Delete mujeres.js

--- mujeres.js
+++
@@ -1,6 +0,0 @@
-// Añadir la clase 'mobile-view' al body cuando el ancho de la pantalla es menor de 768px
-if (window.innerWidth < 768) {
-    document.body.classList.add('mobile-view');
-} else {
-    document.body.classList.remove('mobile-view');
-}
